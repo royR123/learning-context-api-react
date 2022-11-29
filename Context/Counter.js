5e08a8d5eb0d6cd9c3d5df3381a518c5d5a1d9f8
royR123 <108359792+royR123@users.noreply.github.com>
2022-11-29 21:15:53 +0530

    Update Counter.js

--- Context/Counter.js
+++ Context/Counter.js
@@ -5,7 +5,7 @@ export const CounterContext = createContext(null)
 export const CounterProvider = (props) => {
   const [count,setCount] = useState(0);
   return (
-    <CounterContext.Provider value = {{ count , setCount }}>
+    <CounterContext.Provider value = {{ count , setCount }}>// here the value which are accessed by other components , when they access the counter context.
       {props.children}
     </CounterContext.Provider>
   )
